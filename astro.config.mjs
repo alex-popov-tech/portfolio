478e6d8d1893fcfe18c39cf2bfa489da494318f8
Oleksandr Popov <alex.popov.tech@gmail.com>
2024-06-18 01:21:45 +0300

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://portfolio.sashapopov.com",
+  site: "https://alex-popov-tech.github.io",
+  base: "portfolio",
   integrations: [icon()],
 });
